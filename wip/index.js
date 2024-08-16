27ff8c7bd96bd4e8159cb55d963ba5dabef416de
toastybridge <jtan208295@gmail.com>
2024-08-15 23:14:33 -0500

    Repaired loading animation

--- wip/index.js
+++ wip/index.js
@@ -19,8 +19,6 @@ function init() {
   sleep(1000).then(() => { document.getElementById("circle1").style.borderWidth = "250vh"; });
   sleep(1100).then(() => { document.getElementById("circle2").style.borderWidth = "250vh"; });
   sleep(1200).then(() => { document.getElementById("circle3").style.borderWidth = "250vh"; });
-  sleep(500).then(() => {
-  })
 
   // Reveal the page
   sleep(1400).then(() => {
@@ -36,44 +34,4 @@ function init() {
       document.getElementsByClassName("initCircle").item(i).style.display = "none";
     }
   });
-  
-}
-
-function test() {
-  console.log(document.getElementById("bepe").style.color);
-	if (document.getElementById("bepe").style.color == "rgb(0, 153, 0)") {
-		document.getElementById("bepe").style.color = "#990000";
-	}
-	else {
-		document.getElementById("bepe").style.color = "#009900";
-	}
-
-}
-
-function decreaseSize() {
-  Static.width *= 0.5;
-  console.log(Static.width);
-
-  document.getElementById("childdiv").style.width = Static.width + "%";
-  console.log(getComputedStyle(document.getElementById("circle1")).boxShadow)
-}
-
-function redhat() {
-  Static.top += 5;
-  document.getElementById("pfp").style.top = Static.top + "%";
-  document.getElementById("a").innerHTML = Static.top;
-}
-
-function dmsans() {
-  Static.left += 5;
-  document.getElementById("pfp").style.left = Static.left + "%";
-  document.getElementById("b").innerHTML = Static.left;
-}
-
-function europa() {
-  document.getElementById("bio").style.fontFamily = "EuropaRegular";
-  Static.spacing += 20;
-  document.getElementById("bio").style.lineHeight = Static.spacing + "%";
-
-  // alert(document.getElementById("bio").style.lineHeight);
 }
