0d8aca314c8c238a5c2220c6d72095c280206ece
jor-dango <jtan208295@gmail.com>
2026-07-10 16:29:33 -0500

    Delete old files

--- src/pages/Placeholder.tsx
+++
@@ -1,9 +0,0 @@
-function Placeholder() {
-  return (
-    <div style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h3>If you're seeing this, then that means this section of the website is currently under construction. It'll be ready soon! 🚧</h3>
-    </div>
-  )
-}
-
-export default Placeholder
